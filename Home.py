8ce4ba23f1c263d8d0fb86661a45d87d8fa57f5b
Milaskrn <150208921+Milaskrn@users.noreply.github.com>
2025-02-11 16:27:18 +0700

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(
+    page_title="Project Analisis Sentimen",
+    page_icon="📚",
+)
+
+# Judul di tengah dan melebar
+st.markdown(
+    """
+    <div style='margin-left: -300px; width: 200%; text-align: center; font-size: 75px; font-weight: bold;'>
+        Sentimen Analisis Ketertarikan Pembelian Mobil Listrik Suzuki Menggunakan Metode Klasifikasi Logistic Regression
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+
+# Memuat logo Suzuki
+logo = Image.open("suzuki.png")  # Ganti dengan path logo yang sesuai
+
+# Menampilkan logo dan teks di bagian atas sidebar
+with st.sidebar:
+    st.image(logo, use_container_width=True)  # Menampilkan logo di atas
+    st.markdown(
+        "<h1 style='text-align: center; color: black;'>Project nya Miwa San</h1>",
+        unsafe_allow_html=True
+    )
+
+# Mengubah warna sidebar
+st.markdown(
+    """
+    <style>
+        [data-testid="stSidebar"] {
+            background-color: #e4f6ff;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Menampilkan video YouTube di tengah
+st.markdown(
+    """
+    <div style="margin-left: -300px; justify-content: center;">
+        <iframe width="1400" height="800" src="https://www.youtube.com/embed/q1QA0VVSDwY?si=SO_68GHUj72-hIrm" frameborder="0" allowfullscreen></iframe>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+# Menampilkan teks konten penelitian di bawah video
+st.markdown(
+    """
+    <div style='text-align: justify; font-size: 30px; margin-top: 30px; margin-left:-300px; width: 200%;'>
+        <b>Abstrak:</b> 
+        Penelitian ini bertujuan untuk menganalisis sentimen masyarakat terhadap ketertarikan pembelian mobil listrik Suzuki 
+        dengan menggunakan metode klasifikasi Logistic Regression. Dengan meningkatnya minat terhadap kendaraan listrik, 
+        pemahaman terhadap opini publik menjadi penting untuk strategi pemasaran dan pengembangan produk. 
+        Dataset yang digunakan berasal dari berbagai ulasan pengguna di media sosial dan platform e-commerce. 
+        Hasil penelitian ini diharapkan dapat memberikan wawasan mengenai faktor-faktor yang mempengaruhi minat masyarakat 
+        terhadap mobil listrik serta membantu Suzuki dalam merancang kebijakan yang lebih tepat.
+    </div>
+    """,
+    unsafe_allow_html=True
+)
